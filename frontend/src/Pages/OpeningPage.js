27ecb864d4c771908b0797866d4a43dfb22020a7
SangUn Park <Sangunpark1@gmail.com>
2023-09-07 23:42:39 -0400

    added a little intro message that we can alter

--- frontend/src/Pages/OpeningPage.js
+++ frontend/src/Pages/OpeningPage.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom"
 import axios from "axios";
 import "./OpeningPage.css"
 
@@ -39,60 +40,70 @@ const handleFalseChange = () => {
   setIsFalseChecked(true);
 };
 
-function MinusCounter() {
-  progressCounter--
-}
 
-    return (
-    <>
-      <div className="fakeMenu">
-        <div className="fakeButtons fakeClose"></div>
-        <div className="fakeButtons fakeMinimize"></div>
-        <div className="fakeButtons fakeZoom"></div>
+return (
+  <>
+    <div className="fakeMenu">
+      <div className="fakeButtons fakeClose"></div>
+      <div className="fakeButtons fakeMinimize"></div>
+      <div className="fakeButtons fakeZoom"></div>
+    </div>
+    {counter === 0 ? <><div className="fakeScreen">
+      <div className="lineone">
+        Want to learn more about programming and coding? Hit next and to embark on your journey!
+        
+        </div>
+        <br/>
+        <br/>
+        <br/>
       </div>
-      
+      <button onClick={handleIncrement}>Next</button>
+      </> : null}
   
-              {counter === 0 ?<><div className="fakeScreen">
-            <div className="line1"><div>True Or False? Software developers work on teams to complete projects</div><br/><br/><br/> <form>
-            <label>
-        <input
-          type="checkbox"
-          checked={isTrueChecked}
-          onChange={handleTrueChange}
-        />{' '}
-        True
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          checked={isFalseChecked}
-          onChange={handleFalseChange}
-        />{' '}
-        False
-      </label>
-    </form></div>
+    {counter === 1 ?<><div className="fakeScreen">
+      <div className="line1"><div>True Or False? <br/>Software developers completely independently to complete projects.</div><br/><br/><br/> 
+        <form>
+          <label>
+            <input
+              type="checkbox"
+              checked={isTrueChecked}
+              onChange={handleTrueChange}
+            />{' '}
+            True
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              checked={isFalseChecked}
+              onChange={handleFalseChange}
+            />{' '}
+            False
+          </label>
+        </form>
+        </div>
       </div><button onClick={handleIncrement}>Next</button></> : null}
 
-      {counter === 1 ?<><div className="fakeScreen">
-            <div className="line1"><div>True Or False? Developers should use a version tracker to keep track of code changes, in case something goes wrong</div><br/><br/><br/> <form>
-            <label>
-        <input
-          type="checkbox"
-          checked={isTrueChecked}
-          onChange={handleTrueChange}
-        />{' '}
-        True
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          checked={isFalseChecked}
-          onChange={handleFalseChange}
-        />{' '}
-        False
-      </label>
-    </form></div>
-      </div><button onClick={handleIncrement}>Make An Account and Continue Your Progress!</button></> : null}
+    {counter === 2 ?<><div className="fakeScreen">
+      <div className="line1"><div>True Or False? Developers should use a version tracker to keep track of code changes, in case something goes wrong</div><br/><br/><br/> <form>
+        <label>
+          <input
+            type="checkbox"
+            checked={isTrueChecked}
+            onChange={handleTrueChange}
+          />{' '}
+          True
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={isFalseChecked}
+            onChange={handleFalseChange}
+          />{' '}
+          False
+        </label>
+      </form>
+      </div>
+      </div><Link className="signup-button" to={`/signup`}>Continue Your Journey!</Link></> : null}
             
       
 </>
