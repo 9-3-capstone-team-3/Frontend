e391527d4c0474bff4d62da4d86be3512f86e60f
Markeya Mckoy-Carree <mcmcarree@gmail.com>
2023-09-18 15:47:49 -0400

    debugging app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import UserProfile from "./Components/UserProfile";
 import NavBar from "./Components/NavBar";
 import Quiz from "./Components/Quiz";
 
-import BeginnerLevelShowPage from "./Pages/BeginnerLevelShowPage";
+
 import Dashboard from "./Pages/Dashboard";
 
 function App() {
