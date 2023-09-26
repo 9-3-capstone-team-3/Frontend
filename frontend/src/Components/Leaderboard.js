aa5beacc9cb857b5ed7bd165bdeb4766b60d8846
SangUn Park <Sangunpark1@gmail.com>
2023-09-26 10:17:19 -0400

    fixing logic for leaderboard to sort on total poitns

--- frontend/src/Components/Leaderboard.js
+++ frontend/src/Components/Leaderboard.js
@@ -1,26 +1,29 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import LeaderboardItem from "./LeaderboardItem.js";
 import './Leaderboard.css';
 
-export default function Leaderboard(){
+export default function Leaderboard() {
+  const [users, setUsers] = useState([]);
 
-    const [ users, setUsers ] = useState([])
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/users`)
+      .then((res) => {
+        // Sort users by points in descending order
+        const sortedUsers = res.data.sort((a, b) => b.total_points - a.total_points);
+        setUsers(sortedUsers);
+      })
+      .catch((e) => {
+        console.log({ error: e });
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get(`${process.env.REACT_APP_API_URL}/users`).then((res) => {
-            setUsers(res.data)
-        }).catch((e) => {
-            console.log({ error: {e}})
-        })
-    }, []);
-
-    return (
-        <div className="leaderboard">
-          <h1>Leaderboard</h1>
-          {users.map((user, index) => ( // Added 'index' parameter
-            <LeaderboardItem key={user.id} user={user} index={index + 1} /> // Pass the 'index' prop
-          ))}
-        </div>
-      );
+  return (
+    <div className="leaderboard">
+      <h1>Leaderboard</h1>
+      {users.map((user, index) => (
+        <LeaderboardItem key={user.id} user={user} index={index + 1} />
+      ))}
+    </div>
+  );
 }
