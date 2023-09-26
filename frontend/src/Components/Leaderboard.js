045813d81760c005318e5a1a7f9658b655adb2ae
SangUn Park <Sangunpark1@gmail.com>
2023-09-26 17:27:21 -0400

    merge conflict resolved

--- frontend/src/Components/Leaderboard.js
+++ frontend/src/Components/Leaderboard.js
@@ -1,45 +1,40 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react"
 import LeaderboardItem from "./LeaderboardItem.js";
 import './Leaderboard.css';
 import { firestore } from '../services/Firebase.js'
 import { collection, query, getDocs } from "firebase/firestore";
 import { db } from "../services/Firebase.js";
 
+export default function Leaderboard(){
 
-export default function Leaderboard() {
-  const [users, setUsers] = useState([]);
+    const [ users, setUsers ] = useState([])
 
+    useEffect(() => {
 
-    // Query the "users" collection in Firestore
-    const getUsers = async () => {
-      try {
-        const usersCollection = query(collection(db, "users"));
-        const snapshot = await getDocs(usersCollection);
+      // Query the "users" collection in Firestore
+      const getUsers = async () => {
+        try {
+          const usersCollection = query(collection(db, "users"));
+          const snapshot = await getDocs(usersCollection);
+          const usersData = [];
+          snapshot.forEach((doc) => {
+            usersData.push({ id: doc.id, ...doc.data() });
+          });
+          setUsers(usersData);
+        } catch (error) {
+          console.error('Error fetching users:', error);
+        }
+      };
+      getUsers();
+    }, []);
+  
 
-        const usersData = [];
-        snapshot.forEach((doc) => {
-          usersData.push({ id: doc.id, ...doc.data() });
-        });
-
-        setUsers(usersData);
-      } catch (error) {
-        console.error('Error fetching users:', error);
-      }
-    };
-
-    getUsers();
-  }, []);
-
-  console.log(users)
-
-  return (
-    <div className="leaderboard">
-      <h1>Leaderboard</h1>
-      {users.map((user, index) => (
-        <LeaderboardItem key={user.id} user={user} index={index + 1} />
-
-      ))}
-    </div>
-  );
+    return (
+        <div className="leaderboard">
+          <h1>Leaderboard</h1>
+          {users.map((user, index) => ( // Added 'index' parameter
+            <LeaderboardItem key={user.id} user={user} index={index + 1} /> // Pass the 'index' prop
+          ))}
+        </div>
+      );
 }
