070e64c2e724e2e51be33e504d03d76ba6d52f6c
SangUn Park <Sangunpark1@gmail.com>
2023-10-03 09:51:59 -0400

    google auth logic completed, will move to opening page now

--- frontend/src/Components/LogIn.js
+++ frontend/src/Components/LogIn.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { UserContext } from "../providers/userProvider.js";
 import { useNavigate } from "react-router-dom";
 import {
@@ -6,23 +6,93 @@ import {
   logOut
 } from "../services/Firebase.js";
 
+
 export const Login = () => {
-    const navigate = useNavigate();
-    const user = useContext(UserContext);
-     useEffect(() => {
-      if (user) {
-        navigate("/loggedin");
+  const apiUrl = process.env.REACT_APP_API_URL
+  const navigate = useNavigate();
+  const user = useContext(UserContext);
+
+const buildUserObject = (userData) => {
+  const displayName = userData.displayName;
+  const [firstName, lastName] = displayName.split(" ");
+
+  const userObject = {
+    username: displayName,
+    email: userData.email,
+    firstname: firstName,
+    lastname: lastName,
+    auth_id: userData.uid
+  }
+
+  return userObject
+}
+
+const checkUserEmail = async (userData) => {
+
+  try {
+    const response = await fetch(`${apiUrl}/users/email/${userData.email}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    console.log(response)
+    if (response.ok) {
+      // Data successfully sent to the backend
+      console.log('Get Request Succesful');
+      navigate(`/loggedin`);
+    } else {
+      // Handle the error case here
+      console.error('Failed to fetch data');
+      postUserDataToBackend(userData);
+    }
+  } catch (error) {
+    // Handle any network or other errors here
+    console.error('Error checking user email:', error);
+  }
+}
+
+const postUserDataToBackend = async (userData) => {
+    
+    try {
+      const response = await fetch(`${apiUrl}/users`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+      
+      if (response.ok) {
+        // Data successfully sent to the backend
+        console.log('User data sent to backend successfully');
+        navigate(`/loggedin`);
+      } else {
+        // Handle the error case here
+        console.error('Failed to send user data to backend');
       }
-    }, [user]);
-      return (
-      <div>
-        <section>
-          <div>
-            <div> login works</div>
-            <button onClick={signInWithGoogle}>Sign in With google</button>
-            <button onClick={logOut}> sign out</button>
-        </div>
-        </section>
-      </div>
-    );
+    } catch (error) {
+      // Handle any network or other errors here
+      console.error('Error sending user data to backend:', error);
+    }
   };
+
+  useEffect(() => {
+    if (user) {
+      const userOBJ = buildUserObject(user)
+      checkUserEmail(userOBJ); 
+    }
+  }, [user]);
+
+  return (
+    <div>
+      <section>
+        <div>
+          <div> login works</div>
+          <button onClick={signInWithGoogle}>Sign in With google</button>
+          <button onClick={logOut}> sign out</button>
+        </div>
+      </section>
+    </div>
+  );
+};
--- frontend/src/Components/LoggedIn.js
+++ frontend/src/Components/LoggedIn.js
@@ -1,10 +1,16 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../providers/userProvider.js";
 import { useNavigate} from "react-router-dom";
 import { logOut } from "../services/Firebase.js";
 
 
 export const LoggedInPage = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [auth_id, setAuth_id] = useState("");
+  const apiUrl = process.env.REACT_APP_API_URL
     
   const imgStyle = {
       width:'30vh',
@@ -13,11 +19,56 @@ export const LoggedInPage = () => {
 
   const navigate = useNavigate();
   const user = useContext(UserContext);
+
+  const postData = async (event) => {
+    event.preventDefault();
+   
+      const displayName = user.displayName;
+      const [firstName, lastName] = displayName.split(" ");
+      setEmail(user.email);
+      setFirstName(firstName);
+      setLastName(lastName);
+      setAuth_id(user.uid);
+      setUsername(displayName)
+
+      // Send user data to your backend
+      postUserDataToBackend({
+        username: username,
+        email: email,
+        firstname: firstname,
+        lastname: lastname,
+        auth_id: auth_id
+      });
+    }
+  
+
+  const postUserDataToBackend = async (userData) => {
+    try {
+      const response = await fetch(`${apiUrl}/users`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+console.log(response)
+      if (response.ok) {
+        // Data successfully sent to the backend
+        console.log('User data sent to backend successfully');
+      } else {
+        // Handle the error case here
+        console.error('Failed to send user data to backend');
+      }
+    } catch (error) {
+      // Handle any network or other errors here
+      console.error('Error sending user data to backend:', error);
+    }
+  };
   
   useEffect(() => { 
     if(!user) {
         alert("not logged in - redirecting")
-        navigate(`/loggedin`);
+        navigate(`/login`);
       }
     }, [user, navigate]);
 
@@ -40,7 +91,7 @@ export const LoggedInPage = () => {
         </div>
         email: {user.email}
         <button onClick={handleLogout}> LOG OUT</button>
-
+        <button onClick={postData}> POST</button>
       </div>
     );
   } else 
