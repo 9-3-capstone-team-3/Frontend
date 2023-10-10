eddbde22089e2a7daf988666deaeb8667d690e1d
Marangely Rosas <Ladykaliber22@gmail.com>
2023-10-10 18:15:31 -0400

    added official portraits to about us

--- frontend/src/Pages/AboutUs/About.js
+++ frontend/src/Pages/AboutUs/About.js
@@ -1,5 +1,10 @@
 import React from "react";
 import './About.css';
+import ShaniImage from '../../Assests/Shani.jpg';
+import MarangelyImage from '../../Assests/Marangely.jpg';
+import SangImage from '../../Assests/SangUn.jpg';
+import KarimahImage from '../../Assests/KarimahReavis.jpg';
+
 
 
 export default function About() {
@@ -7,7 +12,7 @@ export default function About() {
     const developers = [
         {
             name: 'Shaniqua Coston',
-            image: 'https://avatars.githubusercontent.com/u/105818544?v=4', 
+            image: ShaniImage, 
             gitHub: 'https://github.com/Shani4-1',
             linkedIn: 'https://www.linkedin.com/in/shaniqua-coston/',
             info: 'I am passionate about family, I would love to design software that fosters familial connections.'
@@ -15,23 +20,23 @@ export default function About() {
 
         {
             name: 'Marangely Rosas',
-            image: 'https://media.licdn.com/dms/image/D4E03AQGGonBpy_PxUg/profile-displayphoto-shrink_200_200/0/1690140010234?e=1701907200&v=beta&t=x6IviYe07EXcDd_E-JI-XMmTJwqU7GwsgPdgI_Hpbp0', 
+            image: MarangelyImage, 
             gitHub: 'https://github.com/MarangelyRosas',
             linkedIn: 'https://www.linkedin.com/in/marangelyrosas2022/',
             info: `As a Pursuit Full Stack Web Developer with an entrepreneurial mindset, I'm passionate about building innovative web applications that solve real-world problems.`
         },
 
         {
-            name: 'Sangun Park',
-            image: 'https://avatars.githubusercontent.com/u/115030754?v=4',
+            name: 'SangUn Park',
+            image: SangImage,
             gitHub: 'https://github.com/SangunPark96',
             linkedIn: 'https://www.linkedin.com/in/sangunpark/',
-            info: 'Full Stack Web Dev, Love almost all terrible action/sci-fi movies.'   
+            info: 'Full Stack Web Developer, Love almost all terrible action/sci-fi movies.'   
         },
 
         {
             name: 'Karimah Reavis',
-            image: 'https://avatars.githubusercontent.com/u/115250588?v=4', 
+            image: KarimahImage, 
             gitHub: 'https://github.com/kaycodeandsing',
             linkedIn: 'https://www.linkedin.com/in/karimah-reavis/',
             info: 'Real Estate Agent at Gold Quest Realty Corp., Specializing in commercial realty and great customer service.'
@@ -44,9 +49,11 @@ export default function About() {
             <h1>CodeFusion Developers</h1>
                 <div className="developers">
                     {developers.map((developer, index) => (
+                        
                         <div className="codefusion-developer" key={index}>
                             <h2>{developer.name}</h2>
                             <img src={developer.image} alt={developer.name} />
+                            
                             <p>{developer.info}</p>
                             <div className="social-links">
                                 <button className="social-button">
